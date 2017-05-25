93e9089ee0d721be20a195a455e94460c3ae0d4c
fredrik <fredrik.jadebeck@rwth-aachen.de>
2017-05-26 00:45:14 +0200

    Initial commit to feature/graphics

---
+++ graphics/src/main/java/com/qacwnfq/hinduyama/graphics/App.java
@@ -0,0 +1,13 @@
+package com.qacwnfq.hinduyama.graphics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
